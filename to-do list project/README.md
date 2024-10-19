1df53ca85c733e88e88a1b850759c49125815505
BaoNguyen09 <nttbvlogs@gmail.com>
2024-10-19 15:53:06 -0700

    to-do-list project: finish add task button, working towards check off task feature

--- to-do list project/README.md
+++ to-do list project/README.md
@@ -4,7 +4,9 @@ General:
 - Set up basic UI to view projects, lists, tasks
 
 1. Item
-- Make a button to check off a task
+- Make a button to check off a task (in progress)
+- Add date selection and format them
+- Brush up the UI
 - allow expanding an item when click on it, by default it just show title, priority and due date
 2. List
 3. Project
--- to-do list project/src/index.js
+++ to-do list project/src/index.js
@@ -26,18 +26,50 @@ let items = [item1, item2, item3, item4];
 let divItems = document.querySelector(".items");
 let ulItems = document.createElement("ul");
 
+// List all items in a list
 for (let item of items) {
     let divItem = document.createElement("li");
-    divItem.textContent = item.title;
+    divItem.textContent = `${item.title}; priority: ${item.priority}; note: ${item.description}`;
     divItem.dataset.id = `item: ${item.id}`;
     ulItems.append(divItem);
 }
 divItems.appendChild(ulItems);
 
-let addItem = document.querySelector(".addItem");
-addItem.addEventListener("click", () => {
-    let divItem = document.createElement("li");
-    divItem.textContent = item5.title;
-    divItem.dataset.id = `item: ${item5.id}`;
-    ulItems.append(divItem);
-});
+// function for add item button
+// let addItem = document.querySelector(".addItem");
+// addItem.addEventListener("click", () => {
+//     let divItem = document.createElement("li");
+//     divItem.textContent = item5.title;
+//     divItem.dataset.id = `item: ${item5.id}`;
+//     ulItems.append(divItem);
+// });
+let addItem = () => {
+    // console.log(e);
+    let title = document.querySelector("#item_input").value;
+    if (title) {
+        let priority = document.querySelector("#priority").value;
+        let description = document.querySelector("#description").value;
+        let newItem = new ToDoItem(title, description, "today", priority);
+        let divItem = document.createElement("li");
+        // create checkbox for each item
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.name = "status";
+        checkbox.value = "done";
+        checkbox.onchange = (e) => {
+            console.log(checkbox.value);
+        }
+        divItem.append(checkbox);
+
+        let textNode = document.createTextNode(` ${newItem.title}; priority: ${newItem.priority}; note: ${newItem.description}`);
+
+        divItem.appendChild(textNode);
+        divItem.dataset.id = `item: ${newItem.id}`;
+        ulItems.append(divItem);
+        document.querySelector("input").value = '';
+    }
+    
+};
+
+let btn = document.querySelector("#addItem");
+btn.addEventListener("click", addItem);
